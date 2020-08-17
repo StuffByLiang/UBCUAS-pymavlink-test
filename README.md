fa4595b2e4c839a7c2f7c06006261e79ea5a0d01
Liang Liu <stuffbyliang@gmail.com>
2020-08-16 19:33:24 -0700

    described problem

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 Just testing to see if pymavlink (a low-level mavlink message processing library) can communicate with the autopilot of an unmanned aircraft for UBC UAS realtime
 
+## The stupid problem I'm trying to fix
+Basically, we request data stream from the drone in  `src/telemetry.py` in the `init_data_streams` method.
+
+As multiple messages arrive at the specified frequency, calling `mavlink_connection.recv_match()` only gets the OLDEST ONE. which is a problem.
+
+comment out line 25 `telemetry.start_polling()` in `app.py` to see problem in consoles
+
+Ways to fix
+
+1. Have another thread that constantly reads the data (ie the `poll_for_data` method in `src/telemetry.py`)
+
+2. Somehow flush all other messages
+
 ## Getting Started
 
 ### Prerequisites
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from src.telemetry import Telemetry
 
 # connect to autopilot system
 mavlink_connection = mavutil.mavlink_connection('tcp:164.3.0.3:5760', retries=10)
+# mavlink_connection = mavutil.mavlink_connection('udp:127.0.0.1:14550', retries=10)
 
 # wait for heartbeat from system
 mavlink_connection.wait_heartbeat()
@@ -11,11 +12,38 @@ print("Heartbeat from system (system %u component %u)" % (mavlink_connection.tar
 
 telemetry = Telemetry(mavlink_connection)
 
+# debugging variables
+last_recieved_time = time.time()
+begin_time = time.time()
+time_usec_begin = mavlink_connection.recv_match(type="GPS_RAW_INT", blocking=True).time_usec/1000000
 
-# print recieved data
+"""
+if you comment the following code out, you'll notice that from the main thread's
+GPS_RAW_INT time_usec data does not reflect real time data
+
+"""
+telemetry.start_polling()
+
+# MAIN THREAD LOOP (i want to print out stuff every 2 seconds)
 while True:
-#     try:
-#         print(mavlink_connection.recv_match().to_dict())
-#     except:
-#         pass
-    time.sleep(1)
+    # try: 
+    #     altitude=mavlink_connection.messages['GPS_RAW_INT'].alt  # Note, you can access message fields as attributes!
+    #     timestamp=mavlink_connection.time_since('GPS_RAW_INT')
+    #     print(altitude)
+    #     print(timestamp)
+    # except:
+    #     print('No GPS_RAW_INT message received')
+    
+    try:
+        # print(mavlink_connection.location()) # this helper function calls recv_match(type="GPS_RAW_INT")
+        msg = mavlink_connection.recv_match(type="GPS_RAW_INT", blocking=True)
+        print("\nMain thread msg recieved after " + str(time.time() - last_recieved_time) + " seconds")
+        time_lag = (time.time() - begin_time) - (msg.time_usec/1000000 - time_usec_begin)
+        print(msg) # trys to read GPS_RAW_INT and blocks
+        print("this msg was sent: " + str(time_lag) + " seconds ago from the autopilot")
+        last_recieved_time = time.time()
+        
+    except Exception as e:
+        print(e)
+        pass
+    time.sleep(2) # runs every 2 seconds
